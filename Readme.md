45f2fe3998797d06f767e6768d5e8c8f18ddb444
Jeff Baskin <jeff.l.baskin@gmail.com>
2011-06-03 18:12:03 -0400

    Setup the Jasmine testing, and started to create the constructor.

--- Readme.md
+++ Readme.md
@@ -1,6 +1,7 @@
 # Connect PostgreSQL #
 
 Connect-pg is a middleware session storage for the connect 
-framework using PostgreSQL.
+framework using PostgreSQL.  Why?  Because sometimes you need a 
+relational database handling your data.  
 
 ***
---
+++ spec/connect-pg.spec.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+var PGStore = require('../'),
+	Store = require('connect').session.Store;
+
+describe('connect-pg', function () {
+	it('should have a constructor function', function () {
+		expect(typeof PGStore).toEqual('function');
+	});
+	
+	it('should create an object based on connect\'s Store', function () {
+		var pgStore = new PGStore();
+		var parent = Object.getPrototypeOf(pgStore);
+		expect(Object.getPrototypeOf(parent)).toEqual(Store.prototype);
+	});
+});
