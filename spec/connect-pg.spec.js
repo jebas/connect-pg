9bf85879db97bef0bcdc0c990dcc42fc06ed17ff
Jeff Baskin <jeff.l.baskin@gmail.com>
2011-06-06 17:22:30 -0400

    Added the initial bit for connecting to the PostgreSQL database.

--- spec/connect-pg.spec.js
+++ spec/connect-pg.spec.js
@@ -9,8 +9,22 @@ describe('connect-pg', function () {
 		expect(typeof PGStore).toEqual('function');
 	});
 	
+	it('should throw exception with no options object', function () {
+		expect(function () {
+			var pgStore = new PGStore();
+		}).toThrow();
+	});
+	
+	it('should throw exception for missing pgConnect string', function () {
+		expect(function () {
+			var options = {fred: 'barney'};
+			var pgStore = new PGStore(options);
+		}).toThrow();
+	});
+	
 	it("should create an object based on connect's Store", function () {
-		var pgStore = new PGStore();
+		var options = {pgConnect: "tcp://thetester:password@localhost/pgstore"};
+		var pgStore = new PGStore(options);
 		var parent = Object.getPrototypeOf(pgStore);
 		expect(Object.getPrototypeOf(parent)).toEqual(Store.prototype);
 	});
