de62eb8e9c120b40ad3d87a7c091b8c12b8c9f03
Jeff Baskin <jeff.l.baskin@gmail.com>
2011-06-07 18:22:09 -0400

    Started to layout the set function.

--- spec/connect-pg.spec.js
+++ spec/connect-pg.spec.js
@@ -5,6 +5,11 @@ var PGStore = require('../'),
 	Store = require('connect').session.Store;
 
 describe('connect-pg', function () {
+	beforeEach(function () {
+		this.options = {pgConnect: "tcp://thetester:password@localhost/pgstore"};
+		this.pgStore = new PGStore(this.options);
+	});
+	
 	it('should have a constructor function', function () {
 		expect(typeof PGStore).toEqual('function');
 	});
@@ -23,9 +28,11 @@ describe('connect-pg', function () {
 	});
 	
 	it("should create an object based on connect's Store", function () {
-		var options = {pgConnect: "tcp://thetester:password@localhost/pgstore"};
-		var pgStore = new PGStore(options);
-		var parent = Object.getPrototypeOf(pgStore);
+		var parent = Object.getPrototypeOf(this.pgStore);
 		expect(Object.getPrototypeOf(parent)).toEqual(Store.prototype);
+	});
+	
+	it('should have a set session function', function () {
+		expect(typeof this.pgStore.set).toEqual('function');
 	});
 });
