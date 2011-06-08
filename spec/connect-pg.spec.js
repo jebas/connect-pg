5e4ffdde1cfc5dee58524e1432e22dd14266b38e
Jeff Baskin <jeff.l.baskin@gmail.com>
2011-06-08 18:03:38 -0400

    Still working through the set requirements.  Added callback functionality.

--- spec/connect-pg.spec.js
+++ spec/connect-pg.spec.js
@@ -2,7 +2,8 @@
  * 
  */
 var PGStore = require('../'),
-	Store = require('connect').session.Store;
+	Store = require('connect').session.Store,
+	pg = require('pg');
 
 describe('connect-pg', function () {
 	beforeEach(function () {
@@ -10,29 +11,52 @@ describe('connect-pg', function () {
 		this.pgStore = new PGStore(this.options);
 	});
 	
-	it('should have a constructor function', function () {
-		expect(typeof PGStore).toEqual('function');
+	describe('constructor', function () {
+		it('should have a constructor function', function () {
+			expect(typeof PGStore).toEqual('function');
+		});
+		
+		it('should throw exception with no options object', function () {
+			expect(function () {
+				var pgStore = new PGStore();
+			}).toThrow();
+		});
+		
+		it('should throw exception for missing pgConnect string', function () {
+			expect(function () {
+				var options = {fred: 'barney'};
+				var pgStore = new PGStore(options);
+			}).toThrow();
+		});
+		
+		it("should create an object based on connect's Store", function () {
+			var parent = Object.getPrototypeOf(this.pgStore);
+			expect(Object.getPrototypeOf(parent)).toEqual(Store.prototype);
+		});
 	});
 	
-	it('should throw exception with no options object', function () {
-		expect(function () {
-			var pgStore = new PGStore();
-		}).toThrow();
-	});
-	
-	it('should throw exception for missing pgConnect string', function () {
-		expect(function () {
-			var options = {fred: 'barney'};
-			var pgStore = new PGStore(options);
-		}).toThrow();
-	});
-	
-	it("should create an object based on connect's Store", function () {
-		var parent = Object.getPrototypeOf(this.pgStore);
-		expect(Object.getPrototypeOf(parent)).toEqual(Store.prototype);
-	});
-	
-	it('should have a set session function', function () {
-		expect(typeof this.pgStore.set).toEqual('function');
+	describe('set function', function (){
+		it('should have a set session function', function () {
+			expect(typeof this.pgStore.set).toEqual('function');
+		});
+		
+		it('should call the postgresql database', function () {
+			spyOn(pg, 'connect');
+			this.pgStore.set('fred', {rubble: 'barney'}, function () {});
+			expect(pg.connect).toHaveBeenCalled();
+		});
+		
+		it('should return by using the callback function', function () {
+			var callback = jasmine.createSpy();
+			this.pgStore.set('fred', {rubble: 'barney'}, callback);
+			expect(callback).toHaveBeenCalled();
+		});
+		
+		it('should work without a callback function', function () {
+			var pgStore = this.pgStore;
+			expect(function () {
+				pgStore.set('fred', {rubble: 'barney'});
+			}).not.toThrow();
+		});
 	});
 });
