b3fd73539726839d65b1439c80794eff5ccfa60a
Jeff Baskin <jeff.l.baskin@gmail.com>
2011-06-10 00:07:59 -0400

    Started getting the table and tests put together.

---
+++ lib/session_install.sql
@@ -0,0 +1,5 @@
+-- This installs the table and functions for connect-pg.
+
+create table connect_session (
+	sess_id text primary key
+);
--- spec/pgstore-test.sql
+++ spec/pgstore-test.sql
@@ -2,9 +2,13 @@
 
 begin;
 
-select plan(1);
+select plan(4);
 
-select pass('My test passed.');
+-- Table setup tests.
+select has_table('connect_session', 'There should be a session table.');
+select has_column('connect_session', 'sess_id', 'Needs to have a session id column.');
+select col_type_is('connect_session', 'sess_id', 'text', 'Session id is a string.');
+select col_is_pk('connect_session', 'sess_id', 'The session id is the primary key');
 
 select * from finish();
 
