a5f8198587d45034a785514873f3d7aeddc4616f
Jeff Baskin <jeff.l.baskin@gmail.com>
2011-06-14 14:25:18 -0400

    Cleaned up and isolated the various database tests.

--- spec/pgstore-test.sql
+++ spec/pgstore-test.sql
@@ -19,16 +19,17 @@ select col_type_is('connect_session', 'sess_data', 'text', 'Session data is text
 select has_function('set_session_data', array['text', 'text'], 'Needs a set session data function.');
 
 -- set function adds data
-delete from connect_session;
+select clear_sessions();
 select set_session_data('bedrock', 'Fred Flintstone');
 prepare session_test as select * from connect_session;
 prepare initial_session as values ('bedrock', 'Fred Flintstone');
 select results_eq('session_test', 'initial_session', 'set_session_data needs to enter data.');
 
 -- set function updates data
--- this is dependent on the previous test succeeding.
 prepare session_update as select set_session_data('bedrock', 'Barney Rubble');
 prepare update_session as values ('bedrock', 'Barney Rubble');
+select clear_sessions();
+select set_session_data('bedrock', 'Fred Flintstone');
 select lives_ok('session_update', 'function needs to update as well as insert.');
 select results_eq('session_test', 'update_session', 'set_session needs to update data.');
 
@@ -36,16 +37,18 @@ select results_eq('session_test', 'update_session', 'set_session needs to update
 select has_function('get_session_data', array['text'], 'Needs a get_session_data function.');
 
 -- get function pulls data
--- dependent on previous test.
 prepare session_get as select get_session_data('bedrock');
-prepare bedrock_session_data as values ('Barney Rubble');
+prepare bedrock_session_data as values ('Fred Flintstone');
+select clear_sessions();
+select set_session_data('bedrock', 'Fred Flintstone');
 select results_eq('session_get', 'bedrock_session_data', 'Needs to pull session data.');
 
 -- destroy function tests
 select has_function('destroy_session', array['text'], 'Needs to have a destroy function.');
 
 -- destroy removes data. 
--- This is using previous tests.  
+select clear_sessions();
+select set_session_data('bedrock', 'Fred Flintstone');
 select destroy_session('bedrock');
 select results_eq('session_get', array[null], 'Data needs to be deleted');
 
