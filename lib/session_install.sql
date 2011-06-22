28761e8ceba4c997f1989edc5257e7755481fb9b
Jeff Baskin <jeff.l.baskin@gmail.com>
2011-06-21 22:17:08 -0400

    Moved the database table and functions into their own private schema.

--- lib/session_install.sql
+++ lib/session_install.sql
@@ -1,21 +1,23 @@
 -- This installs the table and functions for connect-pg.
 
-create table connect_session (
+create schema web;
+
+create table web.session (
 	sess_id text primary key,
 	sess_data text,
 	expiration timestamp with time zone default now() + interval '1 day'
 );
 
-create index sess_expiration on connect_session (expiration);
+create index expire_idx on web.session (expiration);
 
-create or replace function set_session_data(
+create or replace function web.set_session_data(
 	sessid text, 
 	sessdata text, 
 	expire timestamp with time zone) 
 returns void as $$
 	begin
 		loop
-			update connect_session 
+			update web.session 
 				set sess_data = sessdata, 
 					expiration = expire 
 				where sess_id = sessid;
@@ -23,7 +25,7 @@ returns void as $$
 				return;
 			end if;
 			begin
-				insert into connect_session (sess_id, sess_data, expiration) 
+				insert into web.session (sess_id, sess_data, expiration) 
 					values (sessid, sessdata, expire);
 				return;
 			exception
@@ -34,62 +36,62 @@ returns void as $$
 	end;
 $$ language plpgsql;
 
-create or replace function get_session_data(sessid text)
+create or replace function web.get_session_data(sessid text)
 returns text as $$
 	declare
 		sessdata text;
 	begin
 		select sess_data into sessdata 
-			from connect_session 
+			from web.session 
 			where sess_id = sessid 
 				and (expiration > now() or expiration isnull);
 		return sessdata;
 	end;
 $$ language plpgsql;
 
-create or replace function destroy_session(sessid text)
+create or replace function web.destroy_session(sessid text)
 returns void as $$
 	begin
-		delete from connect_session where sess_id = sessid;
+		delete from web.session where sess_id = sessid;
 		return;
 	end;
 $$ language plpgsql;
 
-create or replace function clear_sessions()
+create or replace function web.clear_sessions()
 returns void as $$
 	begin
-		delete from connect_session;
+		delete from web.session;
 		return;
 	end;
 $$ language plpgsql;
 
-create or replace function count_sessions()
+create or replace function web.count_sessions()
 returns int as $$
 	declare
 		thecount int := 0;
 	begin
-		select count(*) into thecount from connect_session where expiration > now() or expiration isnull;
+		select count(*) into thecount from web.session where expiration > now() or expiration isnull;
 		return thecount;
 	end;
 $$ language plpgsql;
 
-create or replace function all_session_ids()
+create or replace function web.all_session_ids()
 returns setof text as $$
 	begin
-		return query select sess_id from connect_session where expiration > now() or expiration isnull;
+		return query select sess_id from web.session where expiration > now() or expiration isnull;
 		return;
 	end;
 $$ language plpgsql;
 
-create or replace function remove_expired()
+create or replace function web.remove_expired()
 returns trigger as $$
 	begin
-		delete from connect_session where expiration < now();
+		delete from web.session where expiration < now();
 		return null;
 	end;
 $$ language plpgsql;
 
 create trigger delete_expired_trig
 	after insert or update
-	on connect_session
-	execute procedure remove_expired();
+	on web.session
+	execute procedure web.remove_expired();
--- spec/pgstore-test.sql
+++ spec/pgstore-test.sql
@@ -3,129 +3,134 @@
 -- If this were an MCV setup the database is treated as the model.
 
 begin;
-select plan(31);
+-- \i pgtap.sql
+
+select plan(32);
+
+-- schema tests
+select has_schema('web', 'There should be a web sessions schema.');
 
 -- table tests
-select has_table('connect_session', 'There should be a session table.');
+select has_table('web', 'session', 'There should be a session table.');
 
-select has_column('connect_session', 'sess_id', 'Needs to have a session id column.');
-select col_type_is('connect_session', 'sess_id', 'text', 'Session id is a string.');
-select col_is_pk('connect_session', 'sess_id', 'The session id is the primary key');
+select has_column('web', 'session', 'sess_id', 'Needs to have a session id column.');
+select col_type_is('web', 'session', 'sess_id', 'text', 'Session id is a string.');
+select col_is_pk('web', 'session', 'sess_id', 'The session id is the primary key');
 
-select has_column('connect_session', 'sess_data', 'Needs to store the session data.');
-select col_type_is('connect_session', 'sess_data', 'text', 'Session data is text.');
+select has_column('web', 'session', 'sess_data', 'Needs to store the session data.');
+select col_type_is('web', 'session', 'sess_data', 'text', 'Session data is text.');
 
-select has_column('connect_session', 'expiration', 'Needs a time limit on the session.');
-select col_type_is('connect_session', 'expiration', 'timestamp with time zone', 'expiration needs to be a timestamp.');
-select col_has_default('connect_session', 'expiration', 'Needs a default of + one day.');
-select has_index('connect_session', 'sess_expiration', array['expiration'], 'Needs an index for the expiration column.');
+select has_column('web', 'session', 'expiration', 'Needs a time limit on the session.');
+select col_type_is('web', 'session', 'expiration', 'timestamp with time zone', 'expiration needs to be a timestamp.');
+select col_has_default('web', 'session', 'expiration', 'Needs a default of + one day.');
+select has_index('web', 'session', 'expire_idx', array['expiration'], 'Needs an index for the expiration column.');
 
 -- set function tests
-select has_function('set_session_data', array['text', 'text', 'timestamp with time zone'], 'Needs a set session data function.');
+select has_function('web', 'set_session_data', array['text', 'text', 'timestamp with time zone'], 'Needs a set session data function.');
 
 -- set function adds data
-select clear_sessions();
-select set_session_data('bedrock', 'Fred Flintstone', null);
-prepare session_test as select sess_id, sess_data from connect_session;
+select web.clear_sessions();
+select web.set_session_data('bedrock', 'Fred Flintstone', null);
+prepare session_test as select sess_id, sess_data from web.session;
 prepare initial_session as values ('bedrock', 'Fred Flintstone');
 select results_eq('session_test', 'initial_session', 'set_session_data needs to enter data.');
 
 -- set function updates data
-prepare session_update as select set_session_data('bedrock', 'Barney Rubble', null);
+prepare session_update as select web.set_session_data('bedrock', 'Barney Rubble', null);
 prepare update_session as values ('bedrock', 'Barney Rubble');
-select clear_sessions();
-select set_session_data('bedrock', 'Fred Flintstone', null);
+select web.clear_sessions();
+select web.set_session_data('bedrock', 'Fred Flintstone', null);
 select lives_ok('session_update', 'function needs to update as well as insert.');
 select results_eq('session_test', 'update_session', 'set_session needs to update data.');
 
 -- get function tests
-select has_function('get_session_data', array['text'], 'Needs a get_session_data function.');
+select has_function('web', 'get_session_data', array['text'], 'Needs a get_session_data function.');
 
 -- get function pulls data
-prepare session_get as select get_session_data('bedrock');
+prepare session_get as select web.get_session_data('bedrock');
 prepare bedrock_session_data as values ('Fred Flintstone');
-select clear_sessions();
-select set_session_data('bedrock', 'Fred Flintstone', null);
+select web.clear_sessions();
+select web.set_session_data('bedrock', 'Fred Flintstone', null);
 select results_eq('session_get', 'bedrock_session_data', 'Needs to pull session data.');
 
 -- get function should not pull expired data.
-select clear_sessions();
-select set_session_data('bedrock', 'Fred Flintstone', timestamp '2001-09-13 00:00');
+select web.clear_sessions();
+select web.set_session_data('bedrock', 'Fred Flintstone', timestamp '2001-09-13 00:00');
 select results_eq('session_get', 'values (null)', 'should not retrieve expired data.');
 
 -- destroy function tests
-select has_function('destroy_session', array['text'], 'Needs to have a destroy function.');
+select has_function('web', 'destroy_session', array['text'], 'Needs to have a destroy function.');
 
 -- destroy removes data. 
-select clear_sessions();
-select set_session_data('bedrock', 'Fred Flintstone', null);
-select destroy_session('bedrock');
+select web.clear_sessions();
+select web.set_session_data('bedrock', 'Fred Flintstone', null);
+select web.destroy_session('bedrock');
 select results_eq('session_get', array[null], 'Data needs to be deleted');
 
 -- clear function tests
-select has_function('clear_sessions', 'Needs a clear function.');
+select has_function('web', 'clear_sessions', 'Needs a clear function.');
 
 -- clear removes all data.
-select set_session_data('flintstone', 'fred', null);
-select set_session_data('rubble', 'barney', null);
-select clear_sessions();
-select results_eq('select cast(count(*) as int) from connect_session', 'values (0)', 'There should be no data available.');
+select web.set_session_data('flintstone', 'fred', null);
+select web.set_session_data('rubble', 'barney', null);
+select web.clear_sessions();
+select results_eq('select cast(count(*) as int) from web.session', 'values (0)', 'There should be no data available.');
 
 -- length function tests.
-select has_function('count_sessions', 'Needs a count for the length function.'); 
+select has_function('web', 'count_sessions', 'Needs a count for the length function.'); 
 
 -- length counts the number of records.
-select clear_sessions();
-select set_session_data('flintstone', 'fred', null);
-select set_session_data('rubble', 'barney', null);
-select results_eq('select count_sessions()', 'values (2)', 'This should equal the total number of records.');
+select web.clear_sessions();
+select web.set_session_data('flintstone', 'fred', null);
+select web.set_session_data('rubble', 'barney', null);
+select results_eq('select web.count_sessions()', 'values (2)', 'This should equal the total number of records.');
 
 -- count does not include expired records.
-select clear_sessions();
-select set_session_data('flintstone', 'fred', null);
-select set_session_data('rubble', 'barney', now() + interval '1 day');
-select set_session_data('slade', 'mister', now() - interval '1 day');
-select results_eq('select count_sessions()', 'values (2)', 'This should equal the total number of records.');
+select web.clear_sessions();
+select web.set_session_data('flintstone', 'fred', null);
+select web.set_session_data('rubble', 'barney', now() + interval '1 day');
+select web.set_session_data('slade', 'mister', now() - interval '1 day');
+select results_eq('select web.count_sessions()', 'values (2)', 'This should equal the total number of records.');
 
 -- all function tests.
-select has_function('all_session_ids', 'Needs a listing of all session ids.');
+select has_function('web', 'all_session_ids', 'Needs a listing of all session ids.');
 
 -- test all returns
 prepare session_ids as values ('flintstone'), ('rubble'), ('slade');
-select clear_sessions();
-select set_session_data('flintstone', 'fred', null);
-select set_session_data('rubble', 'barney', null);
-select set_session_data('slade', 'mister', null);
-select results_eq('select all_session_ids()', 'session_ids', 'It should return all ids.');
+select web.clear_sessions();
+select web.set_session_data('flintstone', 'fred', null);
+select web.set_session_data('rubble', 'barney', null);
+select web.set_session_data('slade', 'mister', null);
+select results_eq('select web.all_session_ids()', 'session_ids', 'It should return all ids.');
 
 -- test all returns
 prepare good_session_ids as values ('flintstone'), ('rubble');
-select clear_sessions();
-select set_session_data('flintstone', 'fred', null);
-select set_session_data('rubble', 'barney', now() + interval '1 day');
-select set_session_data('slade', 'mister', now() - interval '1 day');
-select results_eq('select all_session_ids()', 'good_session_ids', 'It should not return expired ids.');
+select web.clear_sessions();
+select web.set_session_data('flintstone', 'fred', null);
+select web.set_session_data('rubble', 'barney', now() + interval '1 day');
+select web.set_session_data('slade', 'mister', now() - interval '1 day');
+select results_eq('select web.all_session_ids()', 'good_session_ids', 'It should not return expired ids.');
 
 -- remove_expired function tests.
-select has_function('remove_expired', 'Needs a function to delete expired records.');
+select has_function('web', 'remove_expired', 'Needs a function to delete expired records.');
 
 -- remove_expired trigger
-select trigger_is('connect_session', 'delete_expired_trig', 'remove_expired', 'clean up trigger should be called.');
+select trigger_is('web', 'session', 'delete_expired_trig', 'web', 'remove_expired', 'clean up trigger should be called.');
 
 -- test remove_expired_trig to delete the old messages.
-select clear_sessions();
-select set_session_data('flintstone', 'fred', null);
-select set_session_data('rubble', 'barney', now() + interval '1 day');
-select set_session_data('slade', 'mister', now() - interval '1 day');
-select results_eq('select sess_id from connect_session', 'good_session_ids', 'It should delete expired sessions.');
+select web.clear_sessions();
+select web.set_session_data('flintstone', 'fred', null);
+select web.set_session_data('rubble', 'barney', now() + interval '1 day');
+select web.set_session_data('slade', 'mister', now() - interval '1 day');
+select results_eq('select sess_id from web.session', 'good_session_ids', 'It should delete expired sessions.');
 
 -- test should remove items after an update.
-select clear_sessions();
-select set_session_data('flintstone', 'fred', null);
-select set_session_data('rubble', 'barney', now() + interval '1 day');
-select set_session_data('slade', 'mister', now() + interval '1 day');
-select set_session_data('slade', 'mister', now() - interval '1 day');
-select results_eq('select sess_id from connect_session', 'good_session_ids', 'It should delete expired sessions after update.');
+select web.clear_sessions();
+select web.set_session_data('flintstone', 'fred', null);
+select web.set_session_data('rubble', 'barney', now() + interval '1 day');
+select web.set_session_data('slade', 'mister', now() + interval '1 day');
+select web.set_session_data('slade', 'mister', now() - interval '1 day');
+select results_eq('select sess_id from web.session', 'good_session_ids', 'It should delete expired sessions after update.');
 
 select * from finish();
 rollback;
